bfd1f5a142cd38df39330f5825fb45618d2c1f1d
Michele Bortone <michelebortone@MacBook-Pro.home>
2020-05-26 18:13:32 +0200

    versione doppia

--- src/main/java/G10HW2.java
+++ src/main/java/G10HW2.java
@@ -78,6 +78,20 @@ class G10HW2 {
             System.out.println("Max distance = " + exactCentersDistance);
             System.out.println("Running time = " + totalTime + " ms");
 
+            //----------------------------------------------------
+            inputPoints = new ArrayList<>(readVectorsSeq(filename));
+
+            startTime = System.nanoTime();
+            ArrayList<Vector> Cz = kCenterMPDZanet(inputPoints, k);
+            double exactCentersDistanceZ = exactMPD(Cz);
+            endTime   = System.nanoTime();
+            totalTime = (endTime - startTime)/1000000;
+
+            System.out.println("\nk-CENTER-BASED ALGORITHM");
+            System.out.println("k = " + k);
+            System.out.println("Max distance = " + exactCentersDistance);
+            System.out.println("Running time = " + totalTime + " ms");
+
         }
         catch(Exception e) {
             e.printStackTrace();
@@ -140,7 +154,7 @@ class G10HW2 {
     }
 
 
-    public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> S, int k) {
+    public static ArrayList<Vector> kCenterMPDZanet(ArrayList<Vector> S, int k) {
         ArrayList<Vector> C = new ArrayList<>();
         ArrayList<Double> distances = new ArrayList<>();
         C.add(S.get(666));
@@ -164,7 +178,81 @@ class G10HW2 {
             C.add(S.get(centerIndex));
             S.remove(centerIndex);
             distances.remove(centerIndex);
+            System.out.println("Indice nuovo centro " + centerIndex);
         }
         return C;
+    }
+
+
+    public static ArrayList<Vector> kCenterMPD(ArrayList<Vector> s, int k)
+    {
+
+        ArrayList<Vector> centers = new ArrayList<>();
+        ArrayList<Double> distances = new ArrayList<>();
+
+        // Add an arbitrary first center (index 0 for example)
+        centers.add(s.get(0));
+        // remove the center from S
+        s.remove(0);
+
+        // For every centroid, track the minimum distances between every point in S and every center
+        // Then we can add the point with the maximum distance from the nearest center
+        for(int i = 0; i < k-1; i++)
+        {
+
+            double max_dist = 0.0;
+            int new_cluster = 0; // flag for tracking what center add next
+
+            for(int j = 0; j < s.size(); j++)
+            {
+                // Calculate the distance between the point and the last centroid added
+                double dist = Vectors.sqdist(s.get(j), centers.get(i));
+
+                // Only first cycle: fill the ArrayList with the distances from the arbitrary center
+                if (i == 0)
+                {
+                    distances.add(dist);
+
+                    // find the second cluster
+                    if (max_dist < distances.get(j))
+                    {
+                        max_dist = distances.get(j);
+                        new_cluster = j;
+                    }
+                }
+
+                // Otherwise
+                else {
+                    // If the distance between the new centroid and the point is less than the previous minimum distance,
+                    // update the minimum distance and check for a possible centroid candidate
+                    if(dist < distances.get(j))
+                    {
+                        distances.set(j, dist);
+                        if (max_dist < distances.get(j)) {
+                            max_dist = distances.get(j);
+                            new_cluster = j;
+                        }
+
+                    }
+                    // Otherwise, check if the previous distance has the maximum distance
+                    else if (max_dist < distances.get(j))
+                    {
+                        max_dist = distances.get(j);
+                        new_cluster = j;
+                    }
+
+                }
+
+            }
+
+            // Add the new centroid to the Arraylist
+            centers.add(s.get(new_cluster));
+            // Remove the centroid from the set S of remaining points and the distance tracker
+            s.remove(new_cluster);
+            distances.remove(new_cluster);
+            System.out.println("Indice nuovo centro di Zanet " + new_cluster);
+
+        }
+        return centers;
     }
 }
